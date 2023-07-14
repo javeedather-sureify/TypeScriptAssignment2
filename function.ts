74a5bec164745bd6ee5d3ec9c039a7ba328862b5
javeedather <javeed.ather@sureify.com>
2023-07-14 13:10:23 +0530

    Initial commit of ts task

---
+++ function.ts
@@ -0,0 +1,22 @@
+function add(a: number, b: number): number {
+    return a + b;
+  }
+  
+  function subtract(a: number, b: number): number {
+    return a - b;
+  }
+  
+  function divide(a: number, b: number): number {
+    if (b === 0) {
+      throw new Error("Division by zero is not allowed.");
+    }
+  
+    return a / b;
+  }
+  
+  function multiply(a: number, b: number): number {
+    return a * b;
+  }
+  
+
+  export { add, subtract, divide, multiply }
---
+++ index.ts
@@ -0,0 +1,5 @@
+import { add, subtract, divide, multiply } from './function';
+console.log(add(5, 3));       // Output: 8
+console.log(subtract(10, 4)); // Output: 6
+console.log(divide(15, 3));   // Output: 5
+console.log(multiply(2, 6));   // Output: 12
